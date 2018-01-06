5b7fbf2bcf7eb965afc340345b9c60e4f07bf151
dariodlearnco <dario.dorella@gmail.com>
2018-01-06 02:41:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,4 +37,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 
-function removeElementFromEndOfArray() {}
+function removeElementFromEndOfArray(array) {
+    return array.slice(0,array.length - 1)
+}
