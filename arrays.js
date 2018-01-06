0b73afd8482584017937a6e6d6c57c5d1fc1d0b9
dariodlearnco <dario.dorella@gmail.com>
2018-01-06 02:26:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(a, b) {
+    return [a, ...b]
+} 
+
+function destructivelyAddElementToBeginningOfArray(a, b) {
+    a=[a, ...b]
+    return a
+} 
