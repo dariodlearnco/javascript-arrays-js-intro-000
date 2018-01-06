ce5bd8fc3d9eaf19ab70be6922436a4587f98342
dariodlearnco <dario.dorella@gmail.com>
2018-01-06 02:34:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,3 +17,13 @@ function destructivelyAddElementToEndOfArray(array, element) {
     array.push(element)
     return array
 }
+
+function accessElementInArray(array, index) {
+    return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {}
+
+function removeElementFromBeginningOfArray(array) {}
+
+
